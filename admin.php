f8012edf6a96d19c2d4e1ff93e25446af31e061e
rhzain <mrrzzain@gmail.com>
2024-05-24 14:09:28 +0700

    main

---
+++ admin.php
@@ -0,0 +1,8 @@
+<?php
+include 'includes/config.php';
+include 'includes/header.php';
+
+// Implement your admin functionality here
+
+include 'includes/footer.php';
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'includes/header.php'; ?>
+<h2>Welcome to the Fishing Place</h2>
+<p>Book your fishing spot now!</p>
+<?php include 'includes/footer.php'; ?>
